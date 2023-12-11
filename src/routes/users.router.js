fa2a550917adb699cd58d1980ab7b383088995d0
Gaston Salvatierra <gastonsalvatierra@icloud.com>
2023-12-11 15:11:09 -0300

    entregable-15

---
+++ src/routes/users.router.js
@@ -0,0 +1,25 @@
+import { Router } from "express";
+const router= Router();
+import { uploader } from "../utils.js";
+
+router.get("/premium/:uid");
+
+
+// Usando Multer
+router.post("/:uid/documents", uploader.fields([
+    { name: 'profile', maxCount: 1 },
+    { name: 'documents', maxCount: 1 },
+    { name: 'products', maxCount: 1 },
+]), (request, response) => {
+    if (!request.files) {
+        return response.status(400).send({ status: "error", mensaje: "No se adjunto archivo." });
+    }else{
+        response.send({ status: "Success", message: "Archivo agregado con exito" });
+    }
+});
+
+
+
+
+
+export default router;
---
+++ src/services/dao/db/models/user.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+const collection = 'users';
+
+const schema = new mongoose.Schema({
+    first_name: String,
+    last_name: String,
+    email: {
+        type: String,
+        unique: true
+    },
+    age: Number,
+    role: {
+        type: String,
+        default: "user"
+    },
+    cart:{
+        type: Array, 
+        default: []
+    },     
+    password: String,
+    documents: [
+        {name: String}, 
+        {reference:String}
+    ],
+    last_connection : {
+        type:Boolean,
+    }
+})
+
+const userModel = mongoose.model(collection, schema);
+
+export default userModel;
